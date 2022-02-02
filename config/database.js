77c04e984a9aeef22378d2e83fcbe72dc28d00ed
Douglas Pires <douglas.pires@live.com>
2022-02-01 23:29:36 -0300

    chore

--- config/database.js
+++ config/database.js
@@ -1,19 +1,15 @@
-const parse = require("pg-connection-string").parse;
-const config = parse(process.env.DATABASE_URL);
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: "sqlite",
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false,
-      },
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
     },
-    debug: false,
+    useNullAsDefault: true,
   },
 });
--- config/server.js
+++ config/server.js
@@ -1,3 +1,7 @@
 module.exports = ({ env }) => ({
-  url: env("MY_HEROKU_URL"),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  app: {
+    keys: env.array("APP_KEYS"),
+  },
 });
