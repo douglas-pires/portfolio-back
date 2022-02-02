ac39f75039a1dc0bf1bbe414a833e2938a39a5c8
Douglas Pires <douglas.pires@live.com>
2022-02-01 23:18:04 -0300

    trying

--- config/server.js
+++ config/server.js
@@ -1,7 +1,3 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  app: {
-    keys: env.array('APP_KEYS'),
-  },
+  url: env("MY_HEROKU_URL"),
 });
